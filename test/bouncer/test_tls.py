21adf6e7a44eab9f8fc9c1d48244dc2f7c3a7cf7
mmetc <92726601+mmetc@users.noreply.github.com>
2023-06-12 10:07:22 +0200

    test tls: allowed ou and bouncer registration in client cert (#141)

--- test/bouncer/test_tls.py
+++ test/bouncer/test_tls.py
@@ -1,3 +1,4 @@
+import json
 
 def test_tls_server(crowdsec, certs_dir, api_key_factory, bouncer, cf_cfg_factory):
     """TLS with server-only certificate"""
@@ -45,7 +46,7 @@ def test_tls_server(crowdsec, certs_dir, api_key_factory, bouncer, cf_cfg_factor
             ])
 
 
-def test_tls_mutual(crowdsec, certs_dir, api_key_factory, bouncer, cf_cfg_factory):
+def test_tls_mutual(crowdsec, certs_dir, api_key_factory, bouncer, cf_cfg_factory, bouncer_under_test):
     """TLS with two-way bouncer/lapi authentication"""
 
     lapi_env = {
@@ -70,12 +71,36 @@ def test_tls_mutual(crowdsec, certs_dir, api_key_factory, bouncer, cf_cfg_factor
         port = cs.probe.get_bound_port('8080')
         cfg = cf_cfg_factory()
         cfg['crowdsec_lapi_url'] = f'https://localhost:{port}/'
-        cfg['cert_path'] = (certs / 'bouncer.crt').as_posix()
-        cfg['key_path'] = (certs / 'bouncer.key').as_posix()
         cfg['ca_cert_path'] = (certs / 'ca.crt').as_posix()
+
+        cfg['cert_path'] = (certs / 'agent.crt').as_posix()
+        cfg['key_path'] = (certs / 'agent.key').as_posix()
 
         with bouncer(cfg) as cf:
             cf.wait_for_lines_fnmatch([
+                "*Starting crowdsec-cloudflare-bouncer*",
                 "*Using CA cert*",
-                "*Using cert auth with cert*",
+                "*Using cert auth with cert * and key *",
+                "*API error: access forbidden*",
             ])
+
+        cs.wait_for_log("*client certificate OU (?agent-ou?) doesn't match expected OU (?bouncer-ou?)*")
+
+        cfg['cert_path'] = (certs / 'bouncer.crt').as_posix()
+        cfg['key_path'] = (certs / 'bouncer.key').as_posix()
+
+        with bouncer(cfg) as cf:
+            cf.wait_for_lines_fnmatch([
+                "*Starting crowdsec-cloudflare-bouncer*",
+                "*Using CA cert*",
+                "*Using cert auth with cert * and key *",
+            ])
+
+            # check that the bouncer is registered
+            res = cs.cont.exec_run('cscli bouncers list -o json')
+            assert res.exit_code == 0
+            bouncers = json.loads(res.output)
+            assert len(bouncers) == 1
+            assert bouncers[0]['name'].startswith('@')
+            assert bouncers[0]['auth_type'] == 'tls'
+            assert bouncers[0]['type'] == bouncer_under_test
